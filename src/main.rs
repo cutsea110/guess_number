7c49268ad981e6352e3ce8e21a1f758167148d8c
cutsea110 <cutsea110@gmail.com>
2019-08-22 06:29:00 +0900

    update messages

--- src/main.rs
+++ src/main.rs
@@ -8,32 +8,48 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    // println!("secret number is: {}", secret_number);
+    //println!("secret number is: {}", secret_number);
+
+    let mut count = 1;
 
     loop {
-        println!("Please input your guess.");
+        println!("かずをあててみて");
 
         let mut guess = String::new();
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        io::stdin().read_line(&mut guess).expect("すうじをいれてよ");
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(msg) => {
-                println!("{}", msg);
+            Err(_) => {
+                println!("1から100までのかずしかいれないで (>_<)");
                 continue;
             }
         };
 
-        println!("You guessed: {}", guess);
+        println!("{} かいめ: あなたのよそう: {}", count, guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => {
+                println!("ちいさすぎー! q(6_6)");
+                count += 1;
+            }
+            Ordering::Greater => {
+                println!("おおきすぎー! (6_6)b");
+                count += 1;
+            }
             Ordering::Equal => {
-                println!("You win!");
+                println!(
+                    "あったりー!: {} かいめであてたよ (^-^)v",
+                    count
+                );
+                if count == 7 {
+                    println!("よくがんばったね!");
+                } else if count > 7 {
+                    println!("ざんねん! つぎはもっとがんばろうね.");
+                } else if count < 7 {
+                    println!("すっごーい! やったねー! \\(^o^)/");
+                }
                 break;
             }
         }
